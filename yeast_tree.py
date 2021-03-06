060d9812f338849dcfe6efb2fb2e29cbf54be0c2
mattRicotta <mattRicotta@fakeemail.com>
2021-03-06 13:43:18 -0500

    refined code

--- yeast_tree.py
+++ yeast_tree.py
@@ -24,17 +24,15 @@ with open("yeast_table.csv") as yeast_table_file:
 if not progenitor_id in yeast_records:
   raise SystemExit("Progenitor record ID not found.")
 
+# Build yeast_tree
 yeast_tree = Tree()
 
 # Create first node:
-first_record = yeast_records[progenitor_id]
-yeast_id = first_record['ID']
-yeast_name = get_yeast_name(first_record)
-yeast_tree.create_node(tag=yeast_name, identifier=yeast_id)
+yeast_name = get_yeast_name(yeast_records[progenitor_id])
+yeast_tree.create_node(tag=yeast_name, identifier=progenitor_id)
 
 # Iterate through records and add if Parent_ID is in the tree
-for record in yeast_records.values():
-  yeast_id = record['ID']
+for yeast_id, record in yeast_records.items():
   yeast_name = get_yeast_name(record)
   parent_id = record['Parent_ID']
   if parent_id in yeast_tree.nodes:
