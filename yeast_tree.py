c5063999c0e9c4e61874387daffca8ec64a0d4ce
mattRicotta <mattRicotta@fakeemail.com>
2021-03-06 10:31:36 -0500

    Fixed DB query and updated .csv file

--- yeast_tree.py
+++ yeast_tree.py
@@ -6,15 +6,15 @@
 from treelib import Node, Tree
 import csv
 
-# set ID for progenitor yeast record of interest
-progenitor_id = 562
+# set ID as string for progenitor yeast record of interest
+progenitor_id = '730'
 
 # open file and save to yeast_records list
 yeast_records = {}
 with open("yeast_table.csv") as yeast_table_file:
   yeast_csv = csv.DictReader(yeast_table_file)
   for row in yeast_csv:
-    yeast_records[int(row['ID'])]= row
+    yeast_records[row['ID']]= row
 
 # exit program if progenitor_id is not in the csv file
 if not progenitor_id in yeast_records:
@@ -25,13 +25,13 @@ yeast_tree = Tree()
 # Create first node:
 first_record = yeast_records[progenitor_id]
 yeast_id = first_record['ID']
-yeast_name = first_record['Yeast_Name'] + " (" + str(first_record['Gen']) + ")"
+yeast_name = "G" + str(first_record['Gen']) + ": " + first_record['Yeast_Name']
 yeast_tree.create_node(tag=yeast_name, identifier=yeast_id)
 
 # Iterate through records and add if Parent_ID is in the tree
 for record in yeast_records.values():
   yeast_id = record['ID']
-  yeast_name = record['Yeast_Name'] + " (" + str(record['Gen']) + ")"
+  yeast_name = "G" + str(record['Gen']) + ": " + record['Yeast_Name']
   parent_id = record['Parent_ID']
   if parent_id in yeast_tree.nodes:
     yeast_tree.create_node(tag=yeast_name, identifier=yeast_id, parent=parent_id)
